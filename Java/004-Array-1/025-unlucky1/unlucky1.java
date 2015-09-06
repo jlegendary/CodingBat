1a7a1a6fdcc608256ab6aaa4cd218a65174dc7c2
jlegendary <j.legendary2@gmail.com>
2015-09-06 16:56:54 -0400

    Added 025 - unlucky1 - unlucky1.java

--- Java/004-Array-1/025-unlucky1/unlucky1.java
+++ Java/004-Array-1/025-unlucky1/unlucky1.java
@@ -5,7 +5,6 @@ public boolean unlucky1(int[] nums) {
    for (int i=0; i<=1; i++) {
       if (nums[i]==1 && nums[i+1]==3)
          return true;
-      if (arrLength<3) break;
    }
    
    return nums[arrLength-2]==1 && nums[arrLength-1]==3;
