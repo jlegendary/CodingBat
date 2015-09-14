53da6db70b3694bc72e0ff8860ea39fbe4644f95
jlegendary <j.legendary2@gmail.com>
2015-09-14 11:13:47 -0400

    Added 007 - bobThere - .java

---
+++ Java/007-String-2/007-bobThere/bobThere.java
@@ -0,0 +1,7 @@
+public boolean bobThere(String str) {
+   if (str.length()>=3)
+      for (int i=0; i<str.length()-2; i++)
+         if (str.charAt(i)=='b' && str.charAt(i+2)=='b')
+            return true;
+   return false;
+}
