05a56f873a7c2966537851f481559ad05430ca4f
jlegendary <j.legendary2@gmail.com>
2015-10-06 09:45:08 -0400

    Added 014 - matchUp - .java

--- Java/009-Array-2/015-matchUp/matchUp.java
+++ Java/009-Array-2/015-matchUp/matchUp.java
@@ -1,7 +1,7 @@
 public int matchUp(int[] nums1, int[] nums2) {
    int count = 0;
    
-   for (int i =0; i<nums.length; i++)
+   for (int i =0; i<nums1.length; i++)
       if (nums1[i] != nums2[i]
            && Math.abs(nums1[i]-nums2[i]) <= 2)
          count++;
