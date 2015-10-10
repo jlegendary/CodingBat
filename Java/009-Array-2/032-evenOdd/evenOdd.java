70ed5b637da8b70c654ab9935de3d6e21ad6312f
jlegendary <j.legendary2@gmail.com>
2015-10-10 11:20:36 -0400

    Added 032 - evenOdd - .java

---
+++ Java/009-Array-2/032-evenOdd/evenOdd.java
@@ -0,0 +1,12 @@
+public int[] evenOdd(int[] nums) {
+   int[] res = new int[nums.length];
+   int evenPosit = 0;
+   int oddPosit = res.length-1;
+   
+   for (int i=0; i<nums.length; i++)
+      if (nums[i] % 2 == 0)
+         res[evenPosit++] = nums[i];
+      else
+         res[oddPosit--] = nums[i];
+   return res;
+}
