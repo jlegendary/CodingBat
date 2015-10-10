f470f2db6e45bc4d3940dfb119df6872b22ed824
jlegendary <j.legendary2@gmail.com>
2015-10-10 11:26:39 -0400

    Added 033 - fizzBuzz - .java

---
+++ Java/009-Array-2/033-fizzBuzz/fizzBuzz.java
@@ -0,0 +1,18 @@
+public String[] fizzBuzz(int start, int end) {
+   int dif = end - start;
+   String[] res = new String[dif];
+   int posit = 0;
+   
+   for (int i=start; i<end; i++) {
+      boolean fizz = i % 3 == 0;
+      boolean buzz = i % 5 == 0;
+      
+      if (fizz&&buzz) res[posit] = "FizzBuzz";
+      else if (fizz) res[posit] = "Fizz";
+      else if (buzz) res[posit] = "Buzz";
+      else res[posit] = String.valueOf(i);
+      posit++;
+   }
+   
+   return res;
+}
