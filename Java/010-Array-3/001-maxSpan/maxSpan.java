601e40337b09112f4653bcda1cbc8a999425d2fc
jlegendary <j.legendary2@gmail.com>
2015-10-10 15:46:37 -0400

    Added 001 - maxSpan - .java

---
+++ Java/010-Array-3/001-maxSpan/maxSpan.java
@@ -0,0 +1,13 @@
+public int maxSpan(int[] nums) {
+   if (nums.length>0) {
+      int maxSpan = 1;
+      for (int i=0; i<nums.length; i++)
+         for (int j=nums.length-1; j>i; j--)
+            if (nums[j]==nums[i]) {
+               int count = (j-i)+1;
+               if (count>maxSpan) maxSpan = count;
+               break;
+            }
+         return maxSpan;
+      } else return 0;
+}
