1f8d52c3c7059e2e8b105ff967b165aa26b26fe8
jlegendary <j.legendary2@gmail.com>
2015-10-13 20:07:51 -0400

    Added 003 - fix45 - .java

---
+++ Java/010-Array-3/003-fix45/fix45.java
@@ -0,0 +1,15 @@
+public int[] fix45(int[] nums) {
+   for (int i=0; i<nums.length; i++)
+      if (nums[i]==5 && i==0
+          || nums[i]==5 && nums[i-1]!=4) {
+          int fivePosit = i;
+          for (int j=0; j<nums.length; j++)
+             if (nums[j]==4 && nums[j+1]!=5) {
+                int temp = nums[j+1];
+                nums[j+1] = 5;
+                nums[fivePosit] = temp;
+                break;
+             }
+    }
+    return nums;
+}
