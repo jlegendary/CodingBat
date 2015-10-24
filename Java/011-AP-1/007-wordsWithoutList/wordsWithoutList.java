ceac6cf3b253cf4e4ba62a712537e292e5762dfd
jlegendary <j.legendary2@gmail.com>
2015-10-24 15:16:37 -0400

    Added 007 - wordsWithoutList - .java

---
+++ Java/011-AP-1/007-wordsWithoutList/wordsWithoutList.java
@@ -0,0 +1,7 @@
+public List wordsWithoutList(String[] words, int len) {
+	List result = new ArrayList();
+	for (int i=0; i<words.length; i++)
+	   if (words[i].length() != len)
+	      result.add(words[i]);
+	return result;
+}
